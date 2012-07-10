4812a28a9bd53278d929c6dba6d9b1d6ad857fe5
AITI Student <aiti-student@aiti-085-PC.(none)>
2012-07-10 11:12:05 +0000

    part 2 done

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,32 @@
 from django.db import models
 from django.contrib import admin
 
-
+# Add more models here.
 class Post(models.Model):
-    title  = models.CharField(max_length=60)
+    title = models.CharField(max_length=60)
     body = models.TextField()
     created = models.DateField()
-    updated = models.DateField()
+    updated = models.DateField() 
 
 class Comment(models.Model):
-    
     body = models.TextField()
-    author  = models.CharField(max_length=60)
+    author = models.CharField(max_length=60)
     created = models.DateField()
     updated = models.DateField()
     post = models.ForeignKey(Post,related_name='post')
 
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','created','updated')
+    search=('title','body')
+    list_filter=('title','created')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=('post','author','body','created','updated')
+    list_filter=('author','created')
+
+class CommentInline(admin.TabularInline):
+    model=Comment
 
 
-admin.site.register(Post)
-admin.site.register(Comment)
-
-
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
